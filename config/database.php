ced17574a2b12be515de78cd5b1e7f01bb2f17e3
sarsarfi <sarasarfi79@gmail.com>
2025-06-02 20:48:36 +0330

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'webfinal',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+];
